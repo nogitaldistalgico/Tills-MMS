d1064f88db0a1ef6e897629a9e0e796911bf6616
nichtderbongo <nichtderbongo@hallo.de>
2026-01-12 22:28:26 +0100

    feat: update logo with user provided image

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
     },
     themeColor: "#F5F5F7",
     icons: {
-        icon: '/icon.png?v=6',
-        apple: '/apple-icon.png?v=6',
+        icon: '/icon.png?v=7',
+        apple: '/apple-icon.png?v=7',
     },
     appleWebApp: {
         capable: true,
